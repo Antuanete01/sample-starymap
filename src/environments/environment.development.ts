81e28997b3f3dc0b1b5396dc65aa993191d23559
antuanete <u202310358@upc.edu.pe>
2025-05-15 18:42:23 -0500

    first commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  // Server Base URL for Rest API
+  serverBaseUrl: 'http://localhost:3000/api/v1',
+  coursesEndpointPath: '/courses',
+};
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  // Server Base URL for Rest API
+  serverBaseUrl: 'http://localhost:3000/api/v1',
+  coursesEndpointPath: '/courses',
+};
+
+
+
