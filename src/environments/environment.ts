0cc7bdbfb4115c00406f6deb249dcef80f9ef219
Antuanete01 <u202310358@upc.edu.pe>
2025-05-15 18:59:08 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // Server Base URL for Rest API
   serverBaseUrl: 'http://localhost:3000/api/v1',
-  communityEndpointPath: '/communities',
+  communityEndpointPath: '/assets/data/communities.json',
 };
 
 
